efad9b22a1832ba4549836c41c319e0789a3b0e4
sebastian5482 <88686265+sebastian5482@users.noreply.github.com>
2024-05-20 22:15:38 -0500

    Update database.js

--- node-app/src/database.js
+++ node-app/src/database.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mmongodb+srv://sebaso2411:2382570s@covid.sxxhiiz.mongodb.net/?retryWrites=true&w=majority&appName=Covi/Covid-19', {
-   
+const dbURI = 'mongodb+srv://sebaso2411:2382570s@covid.sxxhiiz.mongodb.net/Covid-19?retryWrites=true&w=majority&appName=Covi';
+
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
-    .then(db => console.log('DB is connected'))
-    .catch(err => console.error(err))
+    .then(() => console.log('DB is connected'))
+    .catch(err => console.error('Database connection error:', err));
